962a1f83c36f86f4f7dbfcebe2fe77bdb9a5d32f
whotopia <9039050+whotopia@users.noreply.github.com>
2023-08-11 20:44:32 +0200

    Update init.rb / Redmine 5.x compatibility
    Proposed change for Redmine 5.x compatibility

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 require 'redmine'
 
-require_dependency 'that_hook'
+Rails.configuration.to_prepare do
+  require_dependency 'that_hook'
+end
 
 Rails.logger.info 'Starting That Email Log plugin for Redmine'
 
